e5d53120365781fe05a087120dbc365aeeb36a06
Anthony Wesley <awl-7@live.com>
2025-07-01 18:10:09 -0300

    fix sliderBar for work in mobile

--- src/constants/publicRoutes.ts
+++ src/constants/publicRoutes.ts
@@ -6,14 +6,14 @@ export const PUBLIC_ROUTES = [
     icon: "carbon:home",
   },
   {
+    href: "/pricing",
+    text: "Planos",
+    icon: "hugeicons:sale-tag-01",
+  },
+  {
     href: "/auth",
     text: "Login",
     // icon: "mdi:podium-gold",
     icon: "simple-icons:authelia",
-  },
-  {
-    href: "/pricing",
-    text: "Planos",
-    icon: "hugeicons:sale-tag-01",
   },
 ];
